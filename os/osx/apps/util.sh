c95904ce8113ee22abbdd4f83e2c082ee66fa42e
hellowor1dn <timothee.flichy@mail.mcgill.ca>
2015-10-25 21:28:37 -0400

    fix: allow all download links
    dont check ssl certificates and disguise as safari 7 user agent

--- os/osx/apps/util.sh
+++ os/osx/apps/util.sh
@@ -2,6 +2,8 @@
 #
 # Utilities for application installation
 
+declare -r CURL_USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"
+
 # -----------------------------------------------------------------------------
 # | Download                                                                   |
 # -----------------------------------------------------------------------------
@@ -12,12 +14,13 @@ download() {
     local url="$2"
 
     if command -v "curl" &> /dev/null; then
-        curl "${url}" -L -s -S -o "${output}" -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A" &> /dev/null
-        # curl -LsSo "${output}" "${url}" &> /dev/null
-        #     │││└─ write output to file
-        #     ││└─ show error messages
-        #     │└─ don't show the progress meter
-        #     └─ follow redirects
+        curl -LsSk -o "${output}" -A "${CURL_USER_AGENT}" "${url}" &> /dev/null
+        #     ││││  │              └─ user agent: disguies curl as a browser
+        #     ││││  └─ output: write output to file
+        #     │││└─ insecure: dont check ssl certificate
+        #     ││└─ error: show error messages upon failure
+        #     │└─ silent: don't show the progress meter
+        #     └─ location: follow redirects
         return $?
     elif command -v "wget" &> /dev/null; then
         wget -qO "${output}" "${url}" &> /dev/null
