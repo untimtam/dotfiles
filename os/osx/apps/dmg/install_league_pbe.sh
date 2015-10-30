d705e42bca55b8953f2743ac7bee3ae324627559
Tim Flichy <timflichy@gmail.com>
2015-10-30 17:20:16 -0400

    install league pbe

---
+++ os/osx/apps/dmg/install_league_pbe.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# Install League
+
+# -----------------------------------------------------------------------------
+# | Errors                                                                     |
+# -----------------------------------------------------------------------------
+
+declare -r E_DOWNLOAD_FAILURE=101
+declare -r E_COPY_FAILURE=102
+declare -r E_REMOVE_FAILURE=103
+
+# -----------------------------------------------------------------------------
+# | Global variables                                                           |
+# -----------------------------------------------------------------------------
+
+# Dowload
+declare -r NAME="League of Legends PBE"
+declare -r URL="http://l3cdn.riotgames.com/PBE/League%20of%20Legends%20PBE.dmg"
+# Dmg
+declare -r VPATH="/Volumes/League of Legends PBE"
+declare -r APP="League of Legends PBE.app"
+
+# -----------------------------------------------------------------------------
+# | Main                                                                       |
+# -----------------------------------------------------------------------------
+
+main() {
+    # switch path to script source
+    cd "$(dirname "${BASH_SOURCE}")" \
+        && source "../../../../script/utils.sh" \
+        && source "../util.sh"
+
+    if [[ -e "/Applications/${NAME}.app" ]]; then
+        print_success "${NAME} already installed"
+        return 0
+    fi
+
+    start_spinner "Downloading ${NAME}"
+    download_dmg "${NAME}" "${URL}"
+    status_stop_spinner "Finished downloading ${NAME}"
+    exit_on_fail "${NAME} download failed" "${E_DOWNLOAD_FAILURE}"
+    cp -R "${VPATH}/${APP}" /Applications
+    status "${NAME} → /Applications" "${E_COPY_FAILURE}"
+    remove_dmg "${NAME}" "${VPATH}"
+    status "Removed ${NAME} archive" "${E_REMOVE_FAILURE}"
+}
+
+main
